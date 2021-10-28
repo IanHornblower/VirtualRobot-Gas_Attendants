2defd01538b96d65a8c02f8374e9b86fe5e633a8
ihornblower <ianmhornblower>
2021-10-28 15:14:52 -0400

    Trajectory.java are now working. Fix Robot centric Waypoint Code also

--- TeamCode/src/org/firstinspires/ftc/teamcode/control/Trajectory.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/control/Trajectory.java
@@ -1,33 +1,46 @@
 package org.firstinspires.ftc.teamcode.control;
 
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import javafx.geometry.Pos;
 import org.firstinspires.ftc.teamcode.hardware.Robot;
 import org.firstinspires.ftc.teamcode.math.Pose2D;
+import org.firstinspires.ftc.teamcode.util.AngleUtil;
 import org.firstinspires.ftc.teamcode.util.Array;
 
 import java.util.ArrayList;
 
-// TODO: Fix absolute mess of a file
+// TODO: Fixed the mess, now the Robot Centric Controls need to be fine tuned.
 
 public class Trajectory {
 
-    Pose2D[] path = new Pose2D[]{};
-
     CornettCore motionProfile;
 
-    public Trajectory (Robot robot) {
+    ArrayList<Pose2D> path = new ArrayList<>();
+
+    Pose2D startPos;
+
+    public Trajectory (Robot robot, Pose2D startPos) {
         this.motionProfile = new CornettCore(robot);
+        this.startPos = startPos;
+
+        path.add(startPos);
     }
 
-    public Pose2D[] get() {
+    public ArrayList<Pose2D> get() {
         return path;
     }
 
     public void addWaypoint(Pose2D waypoint) {
-        Array.appendArray(path, waypoint);
+        path.add(waypoint);
     }
 
+    /**
+     * Always use AngleUtil.interpretAngle(double angle), with this function.
+     * @param angle
+     */
+
     public void addRotation(double angle) {                                                             // May not work (get previous pose form pose array and
-        Array.appendArray(path, new Pose2D(path[path.length-1].x, path[path.length-1].y, angle));       // creates new pose with same xy vector, but new angle
+        path.add(new Pose2D(path.get(path.size()-1).x, path.get(path.size()-1).y, angle));       // creates new pose with same xy vector, but new angle
     }
 
     public void forward(double distance, Robot.controlType style) {
@@ -35,14 +48,14 @@ public class Trajectory {
             default:
                 Pose2D point;
             case FIELD:
-                point = new Pose2D(path[path.length-1].x, path[path.length-1].y+distance, path[path.length-1].heading);
-                Array.appendArray(path, point);
+                point = new Pose2D(path.get(path.size()-1).x, path.get(path.size()-1).y+distance, path.get(path.size()-1).heading);
+                path.add(point);
                 break;
             case ROBOT:
-                double x = distance * Math.cos(path[path.length-1].heading);
-                double y = distance * Math.sin(path[path.length-1].heading);
-                point = new Pose2D(path[path.length-1].x + x, path[path.length-1].y + y, path[path.length-1].heading);
-                Array.appendArray(path, point);
+                double x = distance * Math.cos(path.get(path.size()-1).heading);
+                double y = distance * Math.sin(path.get(path.size()-1).heading);
+                point = new Pose2D(path.get(path.size()-1).x + x, path.get(path.size()-1).y + y, path.get(path.size()-1).heading);
+                path.add(point);
                 break;
         }
     }
@@ -53,14 +66,14 @@ public class Trajectory {
             default:
                 Pose2D point;
             case FIELD:
-                point = new Pose2D(path[path.length-1].x, path[path.length-1].y-distance, path[path.length-1].heading);
-                Array.appendArray(path, point);
+                point = new Pose2D(path.get(path.size()-1).x, path.get(path.size()-1).y-distance, path.get(path.size()-1).heading);
+                path.add(point);
                 break;
             case ROBOT:
-                double x = -distance * Math.cos(path[path.length-1].heading);
-                double y = -distance * Math.sin(path[path.length-1].heading);
-                point = new Pose2D(path[path.length-1].x + x, path[path.length-1].y + y, path[path.length-1].heading);
-                Array.appendArray(path, point);
+                double x = -distance * Math.cos(path.get(path.size()-1).heading);
+                double y = -distance * Math.sin(path.get(path.size()-1).heading);
+                point = new Pose2D(path.get(path.size()-1).x + x, path.get(path.size()-1).y + y, path.get(path.size()-1).heading);
+                path.add(point);
                 break;
         }
     }
@@ -70,14 +83,14 @@ public class Trajectory {
             default:
                 Pose2D point;
             case FIELD:
-                point = new Pose2D(path[path.length-1].x + distance, path[path.length-1].y, path[path.length-1].heading);
-                Array.appendArray(path, point);
+                point = new Pose2D(path.get(path.size()-1).x + distance, path.get(path.size()-1).y, path.get(path.size()-1).heading);
+                path.add(point);
                 break;
             case ROBOT:
-                double x = distance * Math.cos(path[path.length-1].heading - Math.PI/2.0);
-                double y = distance * Math.sin(path[path.length-1].heading - Math.PI/2.0);
-                point = new Pose2D(path[path.length-1].x + x, path[path.length-1].y + y, path[path.length-1].heading);
-                Array.appendArray(path, point);
+                double x = distance * Math.cos(path.get(path.size()-1).heading - Math.PI/2.0);
+                double y = distance * Math.sin(path.get(path.size()-1).heading - Math.PI/2.0);
+                point = new Pose2D(path.get(path.size()-1).x + x, path.get(path.size()-1).y + y, path.get(path.size()-1).heading);
+                path.add(point);
                 break;
         }
     }
@@ -87,22 +100,22 @@ public class Trajectory {
             default:
                 Pose2D point;
             case FIELD:
-                point = new Pose2D(path[path.length-1].x, path[path.length-1].y+distance, path[path.length-1].heading);
-                Array.appendArray(path, point);
+                point = new Pose2D(path.get(path.size()-1).x, path.get(path.size()-1).y+distance, path.get(path.size()-1).heading);
+                path.add(point);
                 break;
             case ROBOT:
-                double x = distance * Math.cos(path[path.length-1].heading+Math.PI/2.0);
-                double y = distance * Math.sin(path[path.length-1].heading+Math.PI/2.0);
-                point = new Pose2D(path[path.length-1].x + x, path[path.length-1].y + y, path[path.length-1].heading);
-                Array.appendArray(path, point);
+                double x = distance * Math.cos(path.get(path.size()-1).heading+Math.PI/2.0);
+                double y = distance * Math.sin(path.get(path.size()-1).heading+Math.PI/2.0);
+                point = new Pose2D(path.get(path.size()-1).x + x, path.get(path.size()-1).y + y, path.get(path.size()-1).heading);
+                path.add(point);
                 break;
         }
     }
 
     public void runPath(double allowableDistanceError) throws InterruptedException {
-        double pathLength = path.length;
+        double pathLength = path.size();
         for(int i = 0; i < pathLength; i++) {
-            motionProfile.runToPositionSync(path[i].x, path[i].y, path[i].heading, allowableDistanceError);
+            motionProfile.runToPositionSync(path.get(i).getX(), path.get(i).getY(), path.get(i).getHeading(), allowableDistanceError);
         }
     }
 
--- TeamCode/src/org/firstinspires/ftc/teamcode/opmodes/TestAuto.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/opmodes/TestAuto.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import javafx.geometry.Pos;
 import org.firstinspires.ftc.teamcode.control.CornettCore;
 import org.firstinspires.ftc.teamcode.control.Trajectory;
 import org.firstinspires.ftc.teamcode.hardware.Robot;
@@ -10,11 +11,13 @@ import org.firstinspires.ftc.teamcode.math.Curve;
 import org.firstinspires.ftc.teamcode.math.Point;
 import org.firstinspires.ftc.teamcode.math.Pose2D;
 import org.firstinspires.ftc.teamcode.util.AngleUtil;
+import org.firstinspires.ftc.teamcode.util.Array;
 import org.firstinspires.ftc.teamcode.util.Controller;
 import static org.firstinspires.ftc.teamcode.util.Controller.*;
 import static org.firstinspires.ftc.teamcode.util.MathUtil.roundPlaces;
 
 import org.firstinspires.ftc.teamcode.util.MathUtil;
+import sun.nio.cs.ext.MacHebrew;
 
 @Autonomous(name = "Testing Auto", group = "Testing")
 public class TestAuto extends LinearOpMode {
@@ -23,7 +26,7 @@ public class TestAuto extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Robot robot = new Robot(hardwareMap);
 
-        robot.setSTART_POSITION(new Pose2D(0, 0, AngleUtil.interpretAngle(0)));
+        robot.setSTART_POSITION(new Pose2D(0, 0, AngleUtil.interpretAngle(90)));
 
         waitForStart();
 
@@ -32,7 +35,20 @@ public class TestAuto extends LinearOpMode {
             robot.updateVelocity();
 
             CornettCore motionProfile = new CornettCore(robot);
-            Trajectory path1 = new Trajectory(robot);
+            Trajectory path1 = new Trajectory(robot, robot.START_POSITION);
+
+            //path1.addWaypoint(new Pose2D(20, 20, Math.toRadians(90)));
+
+            path1.addRotation(AngleUtil.interpretAngle(45));
+
+            //path1.forward(10, Robot.controlType.FIELD);
+
+            path1.left(24, Robot.controlType.ROBOT);
+
+            path1.runPath(0.3);
+
+
+            stop();
         }
     }
 }
